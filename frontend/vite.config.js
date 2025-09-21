977dff976b867612859a19df75606c7e87497c9a
Dong Joo Kwon <dongjoo7@outlook.com>
2025-09-21 15:42:46 -0500

    Setup for render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: 'public',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
   resolve: {
     alias: {
